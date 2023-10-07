6bfe204112886d06a3eea183ec7bcd25fa67c149
BUNGA <3010086711@qq.com>
2023-10-07 16:39:18 +0800

    '项目初始化'

---
+++ src/pages/details/details.vue
@@ -0,0 +1,12 @@
+<template>
+  <view>
+    {{ name }}
+  </view>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+const name = ref('积分明细')
+</script>
+
+<style></style>
---
+++ src/pages/my/my.vue
@@ -0,0 +1,12 @@
+<template>
+  <view>
+    {{ name }}
+  </view>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+const name = ref('个人中心')
+</script>
+
+<style></style>
